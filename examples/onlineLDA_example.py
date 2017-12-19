9a7b500bb6d2e6d60920c12f66f1fda566ed2913
Krxsy <wunschc@informatik.uni-freiburg.de>
2017-12-19 13:47:49 +0100

    update example

--- examples/onlineLDA_example.py
+++ examples/onlineLDA_example.py
@@ -14,37 +14,46 @@ Y = np.loadtxt(path + '/example_data/online_lda/online_lda_responses.csv', delim
 # setting up config space:
 param_file = path + '/example_data/online_lda/param-file.txt'
 f = open(param_file, 'rb')
+
 cs = ConfigurationSpace()
 for row in f:
     cs.add_hyperparameter(UniformFloatHyperparameter("%s" %row[0:4].decode('utf-8'), np.float(row[6:9]), np.float(row[10:13]),np.float(row[18:21])))
 param = cs.get_hyperparameters()
 
+
 # create an instance of fanova with data for the random forest and the configSpace
 f = fANOVA(X = X, Y = Y, config_space = cs)
 
 # marginal for first parameter
-p_list = (1, )
+p_list = (0, )
 res = f.quantify_importance(p_list)
 print(res)
 
-print(cs)
 p2_list = ('Col1', 'Col2')
+res2 = f.quantify_importance(p2_list)
+print(res2)
+p2_list = ('Col0', 'Col2')
+res2 = f.quantify_importance(p2_list)
+print(res2)
+p2_list = ('Col1', 'Col0')
 res2 = f.quantify_importance(p2_list)
 print(res2)
 
 # getting the most important pairwise marginals sorted by importance
-best_margs = f.get_most_important_pairwise_marginals(n=3)
-print(best_margs)
+best_p_margs = f.get_most_important_pairwise_marginals(n=3)
+print(best_p_margs)
 
+triplets = f.get_triple_marginals(['Col0','Col1', 'Col2'])
+print(triplets)
 # visualizations:
 
 # directory in which you can find all plots
-plot_dir = path + '/example_data/test_plots'
+plot_dir = path + '/example_data/online_lda'
 # first create an instance of the visualizer with fanova object and configspace
-vis = fanova.visualizer.Visualizer(f, cs)
+vis = fanova.visualizer.Visualizer(f, cs, plot_dir)
 # generating plot data for col0
 mean, std, grid = vis.generate_marginal(0)
-print(mean)
+
 # creating all plots in the directory
-vis.create_all_plots(plot_dir)
+vis.create_all_plots()
 #vis.create_most_important_pairwise_marginal_plots(plot_dir, 3)
