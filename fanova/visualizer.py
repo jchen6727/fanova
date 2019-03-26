300df23e1b06df9afebfda52bee8a1fd4be2447f
Joshua <shukon@users.noreply.github.com>
2019-03-26 17:58:49 +0100

    Feature/improve plots (#83)
    * MAINT clean up visualization, simplify, add comments
    
    * ADD legend, grid and option to plot incumbents

--- fanova/visualizer.py
+++ fanova/visualizer.py
@@ -32,36 +32,37 @@ class Visualizer(object):
         self._y_label = y_label
         self.logger = logging.getLogger(self.__module__ + '.' + self.__class__.__name__)
 
-    def create_all_plots(self, three_d=True,**kwargs):
+    def create_all_plots(self, three_d=True, **kwargs):
         """
         Creates plots for all main effects and stores them into a directory
+        Specifically, all single and pairwise marginals are plotted
 
+        Parameters
+        ----------
+        three_d: boolean
+            whether or not to plot pairwise marginals in 3D-plot
         """
-
-        for i in range(len(self.cs_params)):
-            param = i
-            param_name = self.cs_params[param].name
+        # single marginals
+        for param_idx in range(len(self.cs_params)):
+            param_name = self.cs_params[param_idx].name
             plt.close()
             outfile_name = os.path.join(self.directory, param_name.replace(os.sep, "_") + ".png")
             self.logger.info("creating %s" % outfile_name)
 
             self.plot_marginal(param, show=False, **kwargs)
             plt.savefig(outfile_name)
+
         # additional pairwise plots:
-        dimensions = []
-        for i in range(len(self.cs_params)):
-                dimensions.append(i)
-        combis = list(it.combinations(dimensions,2))
+        dimensions = list(range(len(self.cs_params)))
+        combis = list(it.combinations(dimensions, 2))
         for combi in combis:
-            param_names = []
-            for p in combi:
-                param_names.append(self.cs_params[p].name)
+            param_names = [self.cs_params[p].name for p in combi]
             plt.close()
             param_names = str(param_names)
             param_names = re.sub('[!,@#\'\n$\[\]]', '', param_names)
-            outfile_name = os.path.join(self.directory, str(param_names).replace(" ","_") + ".png")
+            outfile_name = os.path.join(self.directory, str(param_names).replace(" ", "_") + ".png")
             self.logger.info("creating %s" % outfile_name)
-            self.plot_pairwise_marginal(combi, three_d=three_d,**kwargs)
+            self.plot_pairwise_marginal(combi, three_d=three_d, **kwargs)
             plt.savefig(outfile_name)
 
     def generate_pairwise_marginal(self, param_indices, resolution=20):
@@ -76,11 +77,9 @@ class Visualizer(object):
         resolution: int
             Number of samples to generate from the parameter range as
             values to predict
-
         """
         assert len(param_indices) == 2, "You have to specify 2 (different) parameters"
-        grid_list = []
-        param_names = []
+        grid_list, param_names = [], []
         if (isinstance(self.cs_params[param_indices[0]], (CategoricalHyperparameter)) or
             isinstance(self.cs_params[param_indices[1]], (CategoricalHyperparameter))):
             choice_arr = []
@@ -146,62 +145,61 @@ class Visualizer(object):
             Number of samples to generate from the parameter range as
             values to predict
 
+        show: boolean
+            whether to call plt.show() to show plot directly as interactive matplotlib-plot
+
+        three_d: boolean
+            whether or not to plot pairwise marginals in 3D-plot
         """
         assert len(param_list) == 2, "You have to specify 2 (different) parameters"
-        param_names = []
-        param_indices= []
+        param_names, param_indices= [], []
 
-        for p in param_list:
-            if type(p) == str:
-                p = self.cs.get_idx_by_hyperparameter_name(p)
-            param_names.append(self.cs_params[p].name)
-            param_indices.append(p)
+        for p_idx in param_list:
+            if type(p_idx) == str:  # if param_list consists of parameter names
+                p_idx = self.cs.get_idx_by_hyperparameter_name(p_idx)
+            param_names.append(self.cs_params[p_idx].name)
+            param_indices.append(p_idx)
 
         first_is_cat = isinstance(self.cs_params[param_indices[0]], CategoricalHyperparameter)
         second_is_cat = isinstance(self.cs_params[param_indices[1]], CategoricalHyperparameter)
+
+        fig = plt.figure()
+        plt.title('%s and %s' % (param_names[0], param_names[1]))
+
         if first_is_cat or second_is_cat:
-            # One of the two parameters is categorical
+            # At least one of the two parameters is categorical
             choices, zz = self.generate_pairwise_marginal(param_indices, resolution)
             if first_is_cat and second_is_cat:
-                # Both parameters are categorical
-                fig = plt.figure()
-                plt.title('%s and %s' % (param_names[0], param_names[1]))
+                # Both parameters are categorical -> create hotmap
                 plt.imshow(zz, cmap='hot', interpolation='nearest')
                 plt.xticks(np.arange(0, len(choices[0])), choices[0], fontsize=8)
                 plt.yticks(np.arange(0, len(choices[1])), choices[1], fontsize=8)
                 plt.xlabel(param_names[0])
                 plt.ylabel(param_names[1])
                 plt.colorbar().set_label(self._y_label)
-
-                if show:
-                    plt.show()
             else:
-                # Only one of them is categorical
+                # Only one of them is categorical -> create multi-line-plot
                 cat_choices = self.cs_params[param_indices[0]].choices if first_is_cat else self.cs_params[param_indices[1]].choices
-                x_label = param_names[0] if second_is_cat else param_names[1]
 
-                fig = plt.figure()
                 for i, cat in enumerate(cat_choices):
                     plt.plot(zz[i], label='%s' % str(cat))
-                plt.title('%s and %s' % (param_names[0], param_names[1]))
+
                 plt.ylabel(self._y_label)
-                plt.xlabel(x_label)
+                plt.xlabel(param_names[0] if second_is_cat else param_names[1])  # x-axis displays non-categorical
                 plt.legend()
                 plt.tight_layout()
 
         else:
+            # No categoricals -> create 3D-plot
             grid_list, zz = self.generate_pairwise_marginal(param_indices, resolution)
 
             z_min, z_max = zz.min(), zz.max()
             display_xx, display_yy = np.meshgrid(grid_list[0], grid_list[1])
 
-
-            fig = plt.figure()
-
             if three_d:
                 ax = Axes3D(fig)
-                surface = ax.plot_surface(display_xx, display_yy, zz, rstride=1, cstride=1, cmap=cm.jet, linewidth=0,
-                                      antialiased=False)
+                surface = ax.plot_surface(display_xx, display_yy, zz,
+                                          rstride=1, cstride=1, cmap=cm.jet, linewidth=0, antialiased=False)
                 ax.set_xlabel(param_names[0])
                 ax.set_ylabel(param_names[1])
                 ax.set_zlabel(self._y_label)
@@ -219,17 +217,16 @@ class Visualizer(object):
                 plt.ylabel(param_names[1])
                 plt.colorbar()
 
-            plt.title('%s and %s' %(param_names[0], param_names[1]))
+        if show:
+            plt.show()
+        else:
+            interact_dir = os.path.join(self.directory, 'interactive_plots')
+            if not os.path.exists(interact_dir):
+                self.logger.info('creating %s' % interact_dir)
+                os.makedirs(interact_dir)
+            pickle.dump(fig, open(interact_dir + '/%s_%s.fig.pkl' % (param_names[0], param_names[1]), 'wb'))
 
-            if show:
-                plt.show()
-            else:
-                interact_dir = self.directory + '/interactive_plots'
-                self.logger.info('creating %s/interactive_plots' % self.directory)
-                if not os.path.exists(interact_dir):
-                    os.makedirs(interact_dir)
-                pickle.dump(fig, open(interact_dir + '/%s_%s.fig.pkl' %(param_names[0], param_names[1]), 'wb'))
-            return plt
+        return plt
 
     def generate_marginal(self, param, resolution=100):
         """
@@ -285,7 +282,8 @@ class Visualizer(object):
                 std[i] = np.sqrt(v)
             return mean, std, grid
 
-    def plot_marginal(self, param, resolution=100, log_scale=None, show=True):
+    def plot_marginal(self, param, resolution=100, log_scale=None, show=True,
+                      incumbents=None):
         """
         Creates a plot of marginal of a selected parameter
 
@@ -301,21 +299,31 @@ class Visualizer(object):
         log_scale: boolean
             If log scale is required or not. If no value is given, it is
             deduced from the ConfigSpace provided
+
+        show: boolean
+            whether to call plt.show() to show plot directly as interactive matplotlib-plot
+
+        incumbents: List[Configuration]
+            list of ConfigSpace.Configurations that are marked as incumbents
         """
-        if type(param) == str:
-            param = self.cs.get_idx_by_hyperparameter_name(param)
-        param_name = self.cs_params[param].name
+        param_idx = param
+        if type(param_idx) == str:
+            param_idx = self.cs.get_idx_by_hyperparameter_name(param_idx)
+        param_name = self.cs_params[param_idx].name
 
         # check if categorical
-        if isinstance(self.cs_params[param], (CategoricalHyperparameter)):
-            labels= self.cs_params[param].choices
-            categorical_size  = len(self.cs_params[param].choices)
-            mean, std = self.generate_marginal(param)
-            indices = np.arange(1,categorical_size+1, 1)
-            b = plt.boxplot([[x] for x in mean])
-            plt.xticks(indices, labels)
+        if isinstance(self.cs_params[param_idx], (CategoricalHyperparameter)):
+            # PREPROCESS
+            labels = self.cs_params[param_idx].choices
+            categorical_size = len(self.cs_params[param_idx].choices)
+            indices = np.arange(1, categorical_size+1, 1)
+            mean, std = self.generate_marginal(param_idx)
             min_y = mean[0]
             max_y = mean[0]
+
+            # PLOT
+            b = plt.boxplot([[x] for x in mean])
+            plt.xticks(indices, labels)
             # blow up boxes
             for box, std_ in zip(b["boxes"], std):
                 y = box.get_ydata()
@@ -334,24 +342,42 @@ class Visualizer(object):
 
         else:
 
+            # PREPROCESS
             if log_scale is None:
-                log_scale = self.cs_params[param].log
+                log_scale = self.cs_params[param].log or (np.diff(grid).std() > 0.000001)
 
-            mean, std, grid = self.generate_marginal(param, resolution)
+            mean, std, grid = self.generate_marginal(param_idx, resolution)
             mean = np.asarray(mean)
             std = np.asarray(std)
 
             lower_curve = mean - std
             upper_curve = mean + std
 
-            if log_scale or (np.diff(grid).std() > 0.000001):
-                plt.semilogx(grid, mean, 'b')
+            # PLOT
+            if log_scale:
+                if (np.diff(grid).std() > 0.000001):
+                    self.logger.info("It might be better to plot this parameter"
+                                     " '%s' in log-scale.", param_name)
+                plt.semilogx(grid, mean, 'b', label='predicted %s' % self._y_label)
             else:
-                plt.plot(grid, mean, 'b')
-            plt.fill_between(grid, upper_curve, lower_curve, facecolor='red', alpha=0.6)
-            plt.xlabel(param_name)
+                plt.plot(grid, mean, 'b', label='predicted %s' % self._y_label)
+            plt.fill_between(grid, upper_curve, lower_curve, facecolor='red',
+                             alpha=0.6, label='std')
 
+            if incumbents is not None:
+                if not isinstance(incumbents, list):
+                    incumbents = [incumbents]
+                values = [inc[param_name] for inc in incumbents if param_name in inc and inc[param_name] is not None]
+                indices = [(np.abs(np.asarray(grid) - val)).argmin() for val in values]
+                if len(indices) > 0:
+                    plt.scatter(list([grid[idx] for idx in indices]),
+                                list([mean[idx] for idx in indices]),
+                                label='incumbent', c='black', marker='.', zorder=999)
+
+            plt.xlabel(param_name)
             plt.ylabel(self._y_label)
+            plt.grid(True)
+            plt.legend()
             plt.tight_layout()
 
         if show:
@@ -359,24 +385,24 @@ class Visualizer(object):
         else:
             return plt
 
-
-
     def create_most_important_pairwise_marginal_plots(self, params=None, n=20, three_d=True):
         """
         Creates plots of the n most important pairwise marginals of the whole ConfigSpace
 
         Parameters
         ------------
-        params = list
+        params: list
              Contains the selected parameters for pairwise evaluation
         n: int
              The number of most relevant pairwise marginals that will be returned
+        three_d: boolean
+            whether or not to plot pairwise marginals in 3D-plot
 
         """
         if self.fanova._dict:
             most_important_pairwise_marginals = self.fanova.tot_imp_dict
         else:
-            if params:
+            if params is not None:
                 most_important_pairwise_marginals = self.fanova.get_most_important_pairwise_marginals(params=params)
             else:
                 most_important_pairwise_marginals = self.fanova.get_most_important_pairwise_marginals(n=n)
